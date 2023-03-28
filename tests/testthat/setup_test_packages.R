ee2a63f1f27358edb13a1dac233274c0dbc1f268
dgkf <18220321+dgkf@users.noreply.github.com>
2023-03-28 13:21:30 -0400

    trying to reintroduce test cleanup

--- tests/testthat/setup_test_packages.R
+++ tests/testthat/setup_test_packages.R
@@ -18,40 +18,28 @@ dir.create(lib <- tempfile("ct_"), recursive = TRUE)
 
 tests <- normalizePath(testthat::test_path())
 pkg_dirs <- list(
-  examplepkg = system.file("examplepkg", package = "covtracer"),
-  list.obj = file.path(tests, "packages", "list.obj"),
-  no.evaluable.code = file.path(tests, "packages", "no.evaluable.code"),
-  reexport.srcref = file.path(tests, "packages", "reexport.srcref")
+  system.file("examplepkg", package = "covtracer"),
+  file.path(tests, "packages", "list.obj"),
+  file.path(tests, "packages", "no.evaluable.code"),
+  file.path(tests, "packages", "reexport.srcref")
 )
 
 # install our testing packages into a temp directory
+install.packages(
+  as.character(pkg_dirs),
+  type = "source",
+  lib = lib,
+  repos = NULL,
+  dependencies = FALSE,
+  INSTALL_opts = c("--with-keep.source", "--install-tests"),
+  quiet = TRUE
+)
+
 for (i in seq_along(pkg_dirs)) {
-  install.packages(
-    pkg_dirs[[i]],
-    type = "source",
-    lib = lib,
-    repos = NULL,
-    dependencies = FALSE,
-    INSTALL_opts = c("--with-keep.source", "--install-tests"),
-    quiet = TRUE
-  )
+  pkg_dir <- pkg_dirs[[i]]
+  pkg <- basename(pkg_dir)
+
+  cli::cli_li("{.pkg {pkg}} (derived {.code {pkg}_cov}, {.code {pkg}_ns})")
+  assign(paste0(pkg, "_cov"), covr::package_coverage(pkg_dir))
+  assign(paste0(pkg, "_ns"), getNamespace(pkg))
 }
-
-# from inst/examplepkg
-cli::cli_li("{.pkg examplepkg}")
-examplepkg_cov <- covr::package_coverage(pkg_dirs$examplepkg)
-examplepkg_ns <- getNamespace("examplepkg")
-
-# from tests/testthat/packages/list.obj
-cli::cli_li("{.pkg list.obj}")
-list.obj_cov <- covr::package_coverage(pkg_dirs$list.obj)
-list.obj_ns <- getNamespace("list.obj")
-
-# from tests/testthat/packages/no.evaluable.code
-cli::cli_li("{.pkg no.evaluable.code}")
-no.evaluable.code_cov <- covr::package_coverage(pkg_dirs$no.evaluable.code)
-no.evaluable.code_ns <- getNamespace("no.evaluable.code")
-
-# from tests/testthat/packages/reexport.srcref
-cli::cli_li("{.pkg reexport.srcref}")
-reexport.srcref_ns <- getNamespace("reexport.srcref")
